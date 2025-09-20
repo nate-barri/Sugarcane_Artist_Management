51a88524446dbaf65df03ac17c030b8e3e69ace1
lowitbea2 <francisluis.beato.cics@ust.edu.ph>
2025-09-20 15:42:52 +0800

    Setup 3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // Important: make built asset URLs start with /static/
+  base: '/static/',
   server: {
     proxy: {
-      '/api': 'http://127.0.0.1:8000',  // sends React calls to Django
+      '/api': 'http://127.0.0.1:8000',
     },
+  },
+  build: {
+    outDir: 'dist',      // default is fine
+    emptyOutDir: true,
   },
 })
